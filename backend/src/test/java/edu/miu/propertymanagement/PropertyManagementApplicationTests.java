8d8eb9864c6a35208b41b3b6567b45b4225bcdcb
Anıl Özmen <me@anilozmen.com>
2022-11-20 19:07:08 -0600

    Initial Commit

---
+++ backend/src/test/java/edu/miu/propertymanagement/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.propertymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
