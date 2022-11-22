2a5ce995ca7151a8420baac5883280a87dfa98ea
madhavth <madhavth77@gmail.com>
2022-11-21 23:35:17 -0600

    added registration route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { add } from './reducers/counter';
+import {useDispatch, useSelector} from 'react-redux';
+import {add} from './reducers/counter';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Registration from "./Containers/Registration/Registration";
 
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector(state => state.counter.value);
+    // const dispatch = useDispatch();
+    // const count = useSelector(state => state.counter.value);
 
-  return (
-    <div className="App">
-      <span>Count: {count}</span>
-      <button onClick={() => dispatch(add(8))}>Click</button>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+
+            <div className="App">
+                {/*<span>Count: {count}</span>*/}
+                {/*<button onClick={() => dispatch(add(8))}>Click</button>*/}
+            </div>
+
+            <Routes>
+                <Route path='registration' element={<Registration/>}></Route>
+            </Routes>
+            
+        </BrowserRouter>
+    );
 }
 
 export default App;
