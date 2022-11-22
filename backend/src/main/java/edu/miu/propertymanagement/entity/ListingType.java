10264ec831c9f68ee49dc53bbea9ad256c44807e
Anıl Özmen <me@anilozmen.com>
2022-11-21 23:12:00 -0600

    Entities created

--- backend/src/main/java/edu/miu/propertymanagement/entity/ListingType.java
+++ backend/src/main/java/edu/miu/propertymanagement/entity/ListingType.java
@@ -1,2 +1,22 @@
-package edu.miu.propertymanagement.entity;public class ListingType {
+package edu.miu.propertymanagement.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ListingType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private long id;
+
+    @NotBlank(message = "Listing Type Name cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    private String name;
 }
