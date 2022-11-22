4637adceea69084f7d2184cb46c8c837885c7f32
madhavth <madhavth77@gmail.com>
2022-11-22 14:55:45 -0600

    added httpmessage malform exception handler

--- backend/src/main/java/edu/miu/propertymanagement/controller/exception_controller/AuthExceptionController.java
+++ backend/src/main/java/edu/miu/propertymanagement/controller/exception_controller/AuthExceptionController.java
@@ -1,6 +1,7 @@
 package edu.miu.propertymanagement.controller.exception_controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +20,11 @@ public class AuthExceptionController {
     public ResponseEntity<ErrorResponse> exception(ErrorException exception) {
         ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
         return ResponseEntity.ok(errorResponse);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> exceptionHttpMessage(HttpMessageNotReadableException exception) {
+        ErrorResponse error = new ErrorResponse(exception.getLocalizedMessage());
+        return ResponseEntity.ok(error);
     }
 }
