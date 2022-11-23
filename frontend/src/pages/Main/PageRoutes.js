cf4ef07f1f8b7bd7fd4d39b5a6fad49c28a35dfb
Anıl Özmen <me@anilozmen.com>
2022-11-23 17:47:12 -0600

    propert detail

--- frontend/src/pages/Main/PageRoutes.js
+++ frontend/src/pages/Main/PageRoutes.js
@@ -1,8 +1,8 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import Login from "../../components/Login/Login";
 import ResetPassword from '../../components/ResetPassword/ResetPassword';
 import ChangePassword from '../../components/ChangePassword/ChangePassword';
-import Homepage from "../../components/Homepage/Homepage";
+
 import Register from "../../components/Register/Register";
 import Properties from "../Properties/Properties";
 import PropertyDetail from "../../components/PropertyDetail/PropertyDetail";
@@ -12,7 +12,7 @@ const PageRoutes = (props) => {
 
     return (
         <Routes>
-            <Route path='/' element={<Homepage />} />
+            <Route path='/' element={<Navigate replace to="/properties" />} />
             <Route path='register' element={<Register />} />
             <Route path='login' element={<Login />} />
             <Route path='reset-password' element={<ResetPassword />} />
