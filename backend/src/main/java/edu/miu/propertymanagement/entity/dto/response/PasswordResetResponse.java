e8c07e1e679c42b4b830b9b3d105580a05ccabe7
Anıl Özmen <me@anilozmen.com>
2022-11-22 22:16:45 -0600

    Password Reset Response dto

--- backend/src/main/java/edu/miu/propertymanagement/entity/dto/response/PasswordResetResponse.java
+++ backend/src/main/java/edu/miu/propertymanagement/entity/dto/response/PasswordResetResponse.java
@@ -1,2 +1,10 @@
-package edu.miu.propertymanagement.entity.dto.response;public class PasswordResetResponse {
+package edu.miu.propertymanagement.entity.dto.response;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetResponse {
+    private String token;
 }
