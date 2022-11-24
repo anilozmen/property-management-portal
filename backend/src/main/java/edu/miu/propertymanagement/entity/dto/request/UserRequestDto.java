fc51bb175dda50afc65346622435a93e7c71afae
Anıl Özmen <me@anilozmen.com>
2022-11-24 13:58:25 -0600

    New Dtos for User

---
+++ backend/src/main/java/edu/miu/propertymanagement/entity/dto/request/UserRequestDto.java
@@ -0,0 +1,14 @@
+package edu.miu.propertymanagement.entity.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+    private long id;
+    private boolean deleted;
+}
