f085758dff0fac2aadd02c15229ba6e805ed69d7
Ranjan Paudel <paudeljimrranjan@gmail.com>
2022-11-23 22:27:51 -0600

    Fix modelmapper config

--- backend/src/main/java/edu/miu/propertymanagement/config/ModelMapperConfig.java
+++ backend/src/main/java/edu/miu/propertymanagement/config/ModelMapperConfig.java
@@ -1,7 +1,9 @@
 package edu.miu.propertymanagement.config;
 
+import edu.miu.propertymanagement.entity.Offer;
 import edu.miu.propertymanagement.entity.Property;
 import edu.miu.propertymanagement.entity.dto.response.ListingPropertyDto;
+import edu.miu.propertymanagement.entity.dto.response.OfferDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +17,15 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
 
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
         modelMapper.typeMap(Property.class, ListingPropertyDto.class)
                 .addMappings(mapper -> {
                     mapper.map(src -> src.getPropertyAttributes(), ListingPropertyDto::setPropertyAttributesBasicDto);
+                });
+        modelMapper.typeMap(Offer.class, OfferDto.class)
+                .addMappings(mapper -> {
+                    mapper.map(src -> src.getProperty().getPropertyType(), OfferDto::setPropertyType);
+                    mapper.map(src -> src.getProperty().getPropertyImages(), OfferDto::setPropertyImage);
                 });
 
         return modelMapper;
